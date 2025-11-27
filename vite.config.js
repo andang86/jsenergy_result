eafeb3740a0498dc4e869c240b99b60dd6219656
andang86 <andang86@gmail.com>
2025-11-27 22:40:39 +0900

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/jsenergy_result/',   // GitHub Pages용 경로
 })
